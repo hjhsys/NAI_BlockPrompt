1678331029c5f202288e2f83a72f878fe0a4b836
hjhsys <hjh980907@naver.com>
2026-09-15 01:29:39 +0900

    chore: bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hjhsys.naiblockprompt"
         minSdk = 23
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
